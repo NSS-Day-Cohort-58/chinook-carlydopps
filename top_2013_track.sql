2072649357051bf5f8c9fbb7af7ad475a978167b
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-11-29 10:24:01 -0600

    Complete queries

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,3 +1,15 @@
+-- Query that shows the most purchased track(s) of 2013 - returns only one of the tracks that has the max purchase amount
+
+SELECT
+    t.name,
+    SUM(il.quantity) as Purchases
+FROM InvoiceLine il
+JOIN Invoice i ON i.InvoiceId = il.InvoiceId
+JOIN Track t ON t.TrackId = il.TrackId
+WHERE i.invoiceDate LIKE "2013%" 
+GROUP BY t.name
+ORDER BY Purchases DESC LIMIT 1;
+
 -- Query that shows the most purchased track(s) of 2013 - returns all tracks that have the max purchase amount
 
 SELECT
